b58d3bb5b5f771ec0f9be410077117c402a7d7dd
despean <despean15@gmail.com>
2017-04-04 03:40:08 +0100

    music coming soon scraping with pagination

--- music/music/spiders/__init__.py
+++ music/music/spiders/__init__.py
@@ -7,10 +7,14 @@ import scrapy
 
 class MusicScraping(scrapy.Spider):
     name = 'music'
-    start_urls =[
-        'https://www.last.fm/music/+coming-soon'
-    ]
-    def parse(self , response):
+
+
+
+    def start_requests(self):
+        yield scrapy.Request('https://www.last.fm/music/+coming-soon', callback=self.parse_music)
+
+
+    def parse_music(self , response):
         for music in response.css('#mantle_skin > div.container.page-content > div > div.col-main > ol>li'):
             yield {
                 'title': music.css('div > h3 > a::text').extract_first(),
@@ -18,4 +22,7 @@ class MusicScraping(scrapy.Spider):
                 'image': music.css('div > img::attr(src)').extract_first(),
                 'tags': music.css('div > ul > li > a::text').extract()
             }
-
+        next_page = response.css('.pagination > .next > a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse_music)
